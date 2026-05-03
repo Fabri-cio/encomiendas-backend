e65577edadfa9290f5a18f1173b21c9dc9a4e252
wil <wil.fabri777@gmail.com>
2026-05-03 00:31:17 -0400

    arreglo de las .env de jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,13 +4,17 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule, // Importa el módulo de usuarios para poder usar el servicio
-    JwtModule.register({
-      secret: 'SECRETO_SUPER_SEGURO',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      inject: [ConfigService], // Inyecta el servicio de configuración
+      useFactory: (config: ConfigService) => ({ // se define config como nombre del parámetro
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<number>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   controllers: [AuthController],
