c682ce6d2071da830a3773757c7794ee32f18f29
wil <wil.fabri777@gmail.com>
2026-05-05 22:24:01 -0400

    ssl y migraciones para conectar a render en prod

--- data-source.ts
+++ data-source.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'; //carga variables de entorno desde .env
 import { DataSource } from 'typeorm';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
   synchronize: false,
+  ssl: isProd ? { rejectUnauthorized: false } : false,
 });
